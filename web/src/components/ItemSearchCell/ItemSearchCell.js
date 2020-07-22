194759e9d9d0cf9225666dec209eb02de90f2a2c
Chris Ellis <ellis.cs@gmail.com>
2020-07-22 15:42:44 -0400

    Styling Search bar

--- web/src/components/ItemSearchCell/ItemSearchCell.js
+++ web/src/components/ItemSearchCell/ItemSearchCell.js
@@ -14,5 +14,11 @@ export const Empty = () => <div>Empty</div>
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
 export const Success = ({ itemsByName }) => {
-  return JSON.stringify(itemsByName)
+  return (
+    <div className="absolute w-3/4 mt-16 ml-8 py-2 bg-white shadow-xl rounded-lg">
+      {itemsByName.map(item => {
+        return <a href="#" className="block px-4 py-2 hover:bg-indigo-700 hover:text-white" key={item.id}>{item.name}</a>
+      })}
+    </div>
+  )
 }
--- web/src/components/SearchBar/SearchBar.js
+++ web/src/components/SearchBar/SearchBar.js
@@ -15,7 +15,7 @@ const SearchBar = () => {
   }
 
   return (
-    <div>
+    <div className="w-full flex">
       <form onSubmit={handleSubmit} className="w-full flex md:ml-0" action="#" method="POST">
         <label htmlFor="search_field" className="sr-only">Search</label>
         <div className="relative w-full text-gray-400 focus-within:text-gray-600">
@@ -27,7 +27,7 @@ const SearchBar = () => {
           <input value={query} onChange={handleChange} id="search_field" className="block w-full h-full pl-8 pr-3 py-2 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 sm:text-sm" placeholder="Search" type="search" />
         </div>
       </form>
-      <ItemSearchCell name={query} />
+      {query.length > 0 && <ItemSearchCell name={query} />}
     </div>
   )
 }
