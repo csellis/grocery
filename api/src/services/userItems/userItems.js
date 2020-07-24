ae2fa1d81049450d7f03edd2c28daade004c00f5
Chris Ellis <ellis.cs@gmail.com>
2020-07-24 12:34:44 -0400

    Create userItem in table

--- api/src/services/userItems/userItems.js
+++ api/src/services/userItems/userItems.js
@@ -1,9 +1,25 @@
 import { db } from 'src/lib/db'
+import { getUserServer } from 'src/lib/auth'
 
 export const userItems = () => {
   return db.userItem.findMany()
 }
 
-export const createUserItem = props => {
-  console.log(props)
+export const createUserItem = async props => {
+  const currentUser = await getUserServer()
+  const item = await db.item.findOne({
+    where: { id: props.input.id }
+  })
+
+  const userItem = await db.userItem.create({
+    data: {
+      itemName: item.name,
+      itemId: item.id,
+      picked: false,
+      user: {
+        connect: { id: currentUser.id }
+      }
+    }
+  });
+  return userItem.id;
 }
