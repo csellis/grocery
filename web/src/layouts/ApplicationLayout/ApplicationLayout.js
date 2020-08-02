2d37a33c66de6e57e19daa9d902c99423c153bff
Chris Ellis <ellis.cs@gmail.com>
2020-08-02 15:32:07 -0400

    Remove user email

--- web/src/layouts/ApplicationLayout/ApplicationLayout.js
+++ web/src/layouts/ApplicationLayout/ApplicationLayout.js
@@ -155,9 +155,8 @@ const ApplicationLayout = ({ children }) => {
             --> */}
                 <div className={`origin-topRight ${profileOpen ? 'absolute' : 'hidden'} right-0 mt-2 w-48 rounded-md shadow-lg`}>
                   <div className="py-1 rounded-md bg-white shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                    {isAuthenticated && currentUser && (
+                    {isAuthenticated && (
                       <>
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150" role="menuitem">{currentUser.email}</a>
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150" role="menuitem">Your Profile</a>
                         <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150" role="menuitem">Settings</a>
                       </>
