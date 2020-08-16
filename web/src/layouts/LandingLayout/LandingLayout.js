cedf4e2f24b2ffadb0f7d9a2c6b90e7c3338905c
Chris Ellis <ellis.cs@gmail.com>
2020-08-16 16:42:39 -0400

    Fix tailwindUI import and transitions

--- web/src/layouts/LandingLayout/LandingLayout.js
+++ web/src/layouts/LandingLayout/LandingLayout.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { NavLink, Link, routes, navigate } from '@redwoodjs/router'
 import { useAuth } from "@redwoodjs/auth";
+import Transition from "src/components/Transition/transition";
 
 import UserAuthTools from 'src/components/UserAuthTools';
-
 
 const LandingLayout = ({ children }) => {
 
@@ -32,9 +32,6 @@ const LandingLayout = ({ children }) => {
     cacheLocation: 'localstorage',
     audience: process.env.AUTH0_AUDIENCE,
   }
-
-
-
 
   return (
     <div className="relative bg-gray-50 overflow-hidden">
@@ -107,35 +104,46 @@ const LandingLayout = ({ children }) => {
         From: "opacity-100 scale-100"
         To: "opacity-0 scale-95"
     --> */}
-        <div className={`${mobileOpen ? "absolute" : "hidden"} top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden`}>
-          <div className="rounded-lg shadow-md">
-            <div className="rounded-lg bg-white shadow-xs overflow-hidden" role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
-              <div className="px-5 pt-4 flex items-center justify-between">
+        <Transition
+          show={mobileOpen}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 scale-100"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <div className={`absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden`}>
+            <div className="rounded-lg shadow-md">
+              <div className="rounded-lg bg-white shadow-xs overflow-hidden" role="menu" aria-orientation="vertical" aria-labelledby="main-menu">
+                <div className="px-5 pt-4 flex items-center justify-between">
+                  <div>
+                    <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" alt="" />
+                  </div>
+                  <div className="-mr-2">
+                    <button onClick={() => setMobileOpen(!mobileOpen)} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Close menu">
+                      <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div className="px-2 pt-2 pb-3">
+                  <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Product</a>
+                  <a href="#" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Features</a>
+                  <a href="#" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Marketplace</a>
+                  <a href="#" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Company</a>
+                </div>
                 <div>
-                  <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" alt="" />
+                  <a onClick={handleLoginClick} href="#" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out" role="menuitem">
+                    {isAuthenticated ? "Log Out" : "Log In"}
+                  </a>
                 </div>
-                <div className="-mr-2">
-                  <button onClick={() => setMobileOpen(!mobileOpen)} type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out" aria-label="Close menu">
-                    <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
-              <div className="px-2 pt-2 pb-3">
-                <a href="#" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Product</a>
-                <a href="#" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Features</a>
-                <a href="#" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Marketplace</a>
-                <a href="#" className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Company</a>
-              </div>
-              <div>
-                <a onClick={handleLoginClick} href="#" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out" role="menuitem">
-                  {isAuthenticated ? "Log Out" : "Log In"}
-                </a>
               </div>
             </div>
           </div>
-        </div>
+        </Transition>
+
 
         <main className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
           <div className="text-center">
