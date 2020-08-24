e7f385b07f5fec433f4fe430ef769dd71b1a5f4c
Chris Ellis <ellis.cs@gmail.com>
2020-08-23 22:33:08 -0400

    Update fn

--- api/src/functions/updateUserOnLogin.js
+++ api/src/functions/updateUserOnLogin.js
@@ -1,9 +1,18 @@
-export const handler = async (event, context) => {
-  console.log("hey world")
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: 'updateUserOnLogin function',
-    }),
-  }
+// export const handler = async (event, context) => {
+//   console.log("hey world")
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({
+//       data: 'updateUserOnLogin function',
+//     }),
+//   }
+// }
+
+module.exports = (req, res) => {
+  console.log("Sent")
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  })
 }
