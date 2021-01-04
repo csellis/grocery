fabfdf640872f4bc124628a7f214cf7cb61349b5
Chris Ellis <ellis.cs@gmail.com>
2021-01-04 11:58:03 +0000

    fix(everythign): reinstalled everything yarn yolo

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2021-01-04T11:11:10.882Z
+// 2021-01-04T11:56:06.261Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
