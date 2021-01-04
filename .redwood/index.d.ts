b5c52208f88b56875f1317addaba704a93773db3
Chris Ellis <ellis.cs@gmail.com>
2021-01-04 11:02:39 +0000

    fix(upgrade rw): upgraded to latest
    upgraded to latest rw, prisma changes
    
    BREAKING CHANGE: schema update structure

---
+++ .redwood/index.d.ts
@@ -0,0 +1,4 @@
+// 2021-01-04T10:56:31.914Z
+/// <reference path="./types/import-dir-schemas.d.ts" />
+/// <reference path="./types/import-dir-services.d.ts" />
+/// <reference path="./types/routes.d.ts" />
